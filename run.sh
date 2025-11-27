eb8999a3e9f02e2727d244394869d44d85bcb157
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-27 05:12:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ echo "IAM token acquired."
 echo "Triggering Schematics APPLY on workspace $WORKSPACE_ID ..."
 
 curl -s -X POST \
-  "https://us-south.schematics.cloud.ibm.com/v1/workspaces/${WORKSPACE_ID}/actions/apply" \
+  "https://private-us-south.schematics.cloud.ibm.com/v1/workspaces/${WORKSPACE_ID}/actions/apply" \
   -H "Authorization: Bearer $TOKEN" \
   -H "Content-Type: application/json" \
   -d '{}' \
